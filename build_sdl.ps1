9ba8355da9e46d0598ede58b9a5264ca31e466e8
Andrei Dergunov <andrederguno@gmail.com>
2024-07-01 01:39:39 +0200

    some edits in CMakeLists for windows + clang, added build_sdl.ps1 to build under Windows

---
+++ build_sdl.ps1
@@ -0,0 +1,16 @@
+# Might need to add clang-cl to PATH like that:
+# C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\
+# or that
+# C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.40.33807\bin\Hostx64\x64\
+# for cl.exe
+$CLANG_COMPILER = "clang-cl.exe"
+$WORKING_DIRECTORY = Get-Location
+
+New-Item -Force -ItemType Directory -Path "$WORKING_DIRECTORY/src/SDL/build"
+Set-Location "$WORKING_DIRECTORY/src/SDL/build"
+
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=$CLANG_COMPILER \
+    -DCMAKE_INSTALL_PREFIX=$WORKING_DIRECTORY
+cmake --build . --config Release
+cmake --build . --target install
