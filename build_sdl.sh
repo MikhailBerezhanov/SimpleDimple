51ba854b7ef663239a52e9f92c991212f8fa70d6
andrey_dergunov <andrei_dergunov@epam.com>
2024-06-30 23:16:42 +0200

    install into lib, include, bin, etc

--- build_sdl.sh
+++ build_sdl.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-WORKING_DIRECTORY="."
+set -e
 
-mkdir -p src/SDL/build && \
-    cd src/SDL/build && \
-    cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_INSTALL_PREFIX=${WORKING_DIRECTORY}/SDL && \
-    cmake --build . --config Release && \
-    cmake --build . --target install
+WORKING_DIRECTORY=`pwd`
+
+mkdir -p src/SDL/build
+cd src/SDL/build
+cmake .. -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_INSTALL_PREFIX=${WORKING_DIRECTORY}
+cmake --build . --config Release
+cmake --build . --target install
