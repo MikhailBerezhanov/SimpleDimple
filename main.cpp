a79048950ea224f0dd9d7c481b038fe32f2802ff
andrey_dergunov <andrei_dergunov@epam.com>
2024-06-30 15:30:21 +0000

    ...

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <SDL2/SDL.h>
  
 // for rendering images and graphics on screen
-#include <SDL2/SDL_image.h>
+// #include <SDL2/SDL_image.h>
  
 // for using SDL_Delay() functions
 #include <SDL2/SDL_timer.h>
