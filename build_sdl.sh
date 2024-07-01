0ef49f523c119734b5675e1e63c2c4e3a6fef4ee
andrey_dergunov <andrei_dergunov@epam.com>
2024-07-01 15:35:20 +0200

    clear SDL builds, add script to create a docker container from cli

--- build_sdl.sh
+++ build_sdl.sh
@@ -6,6 +6,9 @@ WORKING_DIRECTORY=`pwd`
 
 mkdir -p src/SDL/build
 cd src/SDL/build
+# Clear residue from previous builds
+rm -rf ./*
+# Configure, build, install
 cmake .. -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_INSTALL_PREFIX=${WORKING_DIRECTORY}
---
+++ run_in_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# An attempt to reproduce VSCode's Dev Containers extension behaviour
+
+set -e
+
+# Container name
+CONTAINER="sdl-container"
+
+# Build image
+docker build -t "$CONTAINER" .
+
+# Run container
+# -it --rm : interactive mode, remove container after it's stopped
+# --name "$CONTAINER-instance" : container name
+# -v $(pwd):/workspace : map current directory into /workspace
+# -w /workspace : set container WORKDIR
+# $CONTAINER : image to use
+# /bin/bash -c "./build_sdl.sh" : run script at startup
+docker run -it --rm \
+    --name "$CONTAINER-instance" \
+    -v $(pwd):/workspace \
+    -w /workspace \
+    "$CONTAINER" \
+    /bin/bash -c "./build_sdl.sh; /bin/bash"
+    
