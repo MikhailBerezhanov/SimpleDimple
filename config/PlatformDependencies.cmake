8dd646886913e2b4fb3756212bf811d27127c699
andrey_dergunov <andrei_dergunov@epam.com>
2024-07-04 18:57:19 +0200

    setup zlib

--- config/PlatformDependencies.cmake
+++ config/PlatformDependencies.cmake
@@ -1,5 +1,6 @@
 if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
     set(SDL_PREFIX "/SDL/Windows")
+    set(ZLIB_PREFIX "/ZLIB/Windows")
     # On Windows, these 2 libraries are so-called "import libraries"
     # We need them to link against SDL2.dll located in the bin/ folder
     # So we need to use these 2 .libs to link that SDL2.dll
@@ -15,13 +16,16 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
         # libSDL2main.a libSDL2.dll.a
         set(__SDL_LIB_NAMES "SDL2main" "SDL2.dll" "SDL2_image.dll")
     endif()
+    set(__ZLIB_LIB_NAMES "zlibstatic")
     # From sdl2-config --cflags
     set(SDL_DEFINITIONS main=SDL_main)
 else()
     # Unix
     set(SDL_PREFIX "/SDL/Unix")
+    set(ZLIB_PREFIX "/ZLIB/Unix")
     # libSDL2.so
     set(__SDL_LIB_NAMES "SDL2" "SDL2_image") 
+    set(__ZLIB_LIB_NAMES "libz.a")
     # From sdl2-config --cflags
     set(SDL_DEFINITIONS _REENTRANT) 
 endif()
@@ -30,14 +34,19 @@ set(SDL_LIB_DIR "${SDL_PREFIX}/lib")
 set(SDL_BIN_DIR "${SDL_PREFIX}/bin")
 set(SDL_INCLUDE_DIR "${SDL_PREFIX}/include")
 
+set(ZLIB_INCLUDE_DIR "${ZLIB_PREFIX}/include")
+set(ZLIB_LIB_DIR "${ZLIB_PREFIX}/lib")
+
 set(SDL_LIBS "")
 
 # Collect all SDL libs
 foreach(X ${__SDL_LIB_NAMES})
-    find_library(SDL_EL ${X} PATHS ${SDL_LIB_DIR})
+    find_library(SDL_EL ${X} PATHS ${SDL_LIB_DIR} NO_DEFAULT_PATH)
     list(APPEND SDL_LIBS "${SDL_EL}")
     unset(SDL_EL CACHE)
 endforeach()
+
+find_library(ZLIB_LIBS ${__ZLIB_LIB_NAMES} PATHS ${ZLIB_LIB_DIR} NO_DEFAULT_PATH)
 
 # Exported variables:
 # SDL_PREFIX
