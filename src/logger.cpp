48247027839536ec42de1740f9a477523ca8266f
andrey_dergunov <andrei_dergunov@epam.com>
2024-07-04 14:36:32 +0200

    log levels

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,15 +1,21 @@
-
-#include "logger.h"
 #include <ctime>
 #include <iomanip>
 #include <stdexcept>
 
-Logger::Logger(const std::string &prefix, size_t max_depth, bool nostdout)
+#include "logger.h"
+
+// create string representation using redefined macro
+#define LOG_X(name) #name,
+const char *log_levels[] = {_LOG_LEVELS_};
+#undef LOG_X
+
+Logger::Logger(const std::string &prefix, size_t max_debug_verbosity, bool nostdout)
     : m_info_file(prefix + "-info.log")
     , m_err_file(prefix + "-error.log")
     , m_dbg_file(prefix + "-debug.log")
-    , m_level_threshold(max_depth)
+    , m_max_debug_verbosity(max_debug_verbosity)
     , m_no_stdout(nostdout)
+    , m_date_format("%c, %Z")
 {
     if (!m_info_file.is_open())
     {
@@ -25,61 +31,65 @@ Logger::Logger(const std::string &prefix, size_t max_depth, bool nostdout)
     }
 }
 
+void Logger::set_date_format(const char *fmt)
+{
+    m_date_format = fmt;
+}
+
 std::stringstream Logger::get_wrapped_date() const
 {
     auto t = std::time(nullptr);
     std::stringstream ss;
-    ss << "[" << std::put_time(std::localtime(&t), "%c, %Z") << "]";
+    ss << std::put_time(std::localtime(&t), m_date_format.c_str());
     return ss;
 }
 
-void Logger::generic_log(const std::string &message, LOG_MODE mode, size_t level)
+void Logger::log(const std::string &message, LOG_LEVEL level, size_t debug_verbosity)
 {
-    if (level <= m_level_threshold)
+    auto ss = get_wrapped_date();
+    ss << " [" << log_levels[static_cast<size_t>(level)] << "]: " << message << "\n";
+    switch (level)
     {
-        auto ss = get_wrapped_date();
-
-        switch (mode)
+        case LOG_LEVEL::INFO:
         {
-            case LOG_MODE::INFO:
+            if (!m_no_stdout)
+                std::cout << ss.str();
+            m_info_file << ss.str();
+            break;
+        }
+        case LOG_LEVEL::DEBUG:
+        {
+            if (debug_verbosity < m_max_debug_verbosity)
             {
-                ss << " INFO: " << message << "\n";
-                if (!m_no_stdout)
-                    std::cout << ss.str();
-                m_info_file << ss.str();
-                break;
-            }
-            case LOG_MODE::DEBUG:
-            {
-                ss << " DEBUG: " << message << "\n";
                 if (!m_no_stdout)
                     std::cout << ss.str();
                 m_dbg_file << ss.str();
-                break;
             }
-            case LOG_MODE::ERROR:
-            {
-                ss << " ERROR: " << message << "\n";
-                if (!m_no_stdout)
-                    std::cerr << ss.str();
-                m_err_file << ss.str();
-                break;
-            }
+            break;
+        }
+        case LOG_LEVEL::ERROR:
+        case LOG_LEVEL::WARN:
+        case LOG_LEVEL::FATAL:
+        {
+            if (!m_no_stdout)
+                std::cerr << ss.str();
+            m_err_file << ss.str();
+            break;
         }
     }
 }
 
-void Logger::info(const std::string &message, size_t depth)
+void Logger::info(const std::string &message)
 {
-    generic_log(message, LOG_MODE::INFO, depth);
+    log(message, LOG_LEVEL::INFO, 0);
 }
 
-void Logger::error(const std::string &message, size_t depth)
+void Logger::error(const std::string &message)
 {
-    generic_log(message, LOG_MODE::ERROR, depth);
+    log(message, LOG_LEVEL::ERROR, 0);
 }
 
 void Logger::debug(const std::string &message, size_t depth)
 {
-    generic_log(message, LOG_MODE::DEBUG, depth);
+    log(message, LOG_LEVEL::DEBUG, depth);
 }
--- src/logger.h
+++ src/logger.h
@@ -5,34 +5,44 @@
 #include <iostream>
 #include <sstream>
 
+// a trick to automatically create a enum and its string representation
+#define _LOG_LEVELS_                                                                                                                       \
+    LOG_X(INFO)                                                                                                                            \
+    LOG_X(WARN)                                                                                                                            \
+    LOG_X(ERROR)                                                                                                                           \
+    LOG_X(FATAL)                                                                                                                           \
+    LOG_X(DEBUG)
+#define LOG_X(name) name,
+enum class LOG_LEVEL : unsigned int
+{
+    _LOG_LEVELS_
+};
+#undef LOG_X
+
 class Logger
 {
 private:
     std::ofstream m_info_file;
     std::ofstream m_err_file;
     std::ofstream m_dbg_file;
-    size_t m_level_threshold;
+    size_t m_max_debug_verbosity;
     bool m_no_stdout;
-
-    enum class LOG_MODE
-    {
-        INFO,
-        ERROR,
-        DEBUG
-    };
+    std::string m_date_format;
 
     std::stringstream get_wrapped_date() const;
 
-    void generic_log(const std::string &message, LOG_MODE mode, size_t level);
-
 public:
-    Logger(const std::string &prefix, size_t max_depth = 0, bool nostdout = false);
+    Logger(const std::string &prefix, size_t max_debug_verbosity = 0, bool nostdout = false);
     // disable copies
     Logger(const Logger &) = delete;
     Logger &operator=(const Logger &) = delete;
 
-    void info(const std::string &message, size_t depth = 0);
-    void error(const std::string &message, size_t depth = 0);
+    void set_date_format(const char *fmt);
+
+    void log(const std::string &message, LOG_LEVEL level, size_t debug_verbosity = 0);
+
+    void info(const std::string &message);
+    void error(const std::string &message);
     void debug(const std::string &message, size_t depth = 0);
 };
 
