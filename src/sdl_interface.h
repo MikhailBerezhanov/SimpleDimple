36a5b96c6baaa8b4325d2aa20e44fc418b4c1a5d
andrey_dergunov <andrei_dergunov@epam.com>
2024-07-08 12:02:24 +0200

    added sdl_interface .h/.cpp

---
+++ src/sdl_interface.h
@@ -0,0 +1,29 @@
+#ifndef SDL_INTERFACE_H
+#define SDL_INTERFACE_H
+
+#include <string>
+
+namespace SDL {
+    // Interface, handles SDL Window
+    class IWindow {
+        
+    };
+
+    // Interface, handles SDL Renderer
+    class IRenderer {
+
+    };
+
+    // Interface, handles SDL Texture
+    class Texture {
+
+    };
+
+    // Interface, handles SDL Surface
+    class Surface {
+
+    };
+
+};
+
+#endif
