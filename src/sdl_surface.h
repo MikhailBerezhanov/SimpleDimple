28b7d2691cfafbb05fdf0a002610024c80d2b6f2
andrey_dergunov <andrei_dergunov@epam.com>
2024-07-08 18:50:34 +0200

    surface sceleton

--- src/sdl_surface.h
+++ src/sdl_surface.h
@@ -1,12 +1,18 @@
 #ifndef SDL_SURFACE_H
 #define SDL_SURFACE_H
 
-#include "sdl_surface.h"
+#include "sdl_interface.h"
 #include "sdl.h"
 
 namespace GameEngine
 {
-
+    class Surface : public ISurface
+    {
+        SDL_Surface *m_surface;
+    public:
+        Surface(const std::string &image_filename);
+        ~Surface();
+    };
 };
 
 #endif
