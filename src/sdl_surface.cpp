835daf9ba5a92a61e4189afbd30fc53cc7eda15e
andrey_dergunov <andrei_dergunov@epam.com>
2024-07-08 18:50:34 +0200

    surface sceleton

--- src/sdl_surface.cpp
+++ src/sdl_surface.cpp
@@ -1,6 +1,19 @@
 #include <stdexcept>
 
+#include "sdl_surface.h"
+
 namespace GameEngine
 {
+    GameEngine::Surface::Surface(const std::string& image_filename) 
+        : m_surface(IMG_Load(image_filename.c_str()))
+    {
+        if (! m_surface) {
+            throw std::runtime_error("Unable to create surface from " + image_filename + ": " + std::string(SDL_GetError()));
+        }
+    }
 
-};
+    Surface::~Surface() 
+    {
+        SDL_FreeSurface(m_surface);
+    }
+    }; // namespace GameEngine
