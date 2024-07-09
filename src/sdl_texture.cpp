97d4f2f8ad0f5a9926eb69c3b5a7a16acd76c5fd
andrey_dergunov <andrei_dergunov@epam.com>
2024-07-09 19:32:38 +0200

    window, render, texture, surface interfaces

--- src/sdl_texture.cpp
+++ src/sdl_texture.cpp
@@ -65,5 +65,24 @@ namespace GameEngine
         }
     }
 
+    std::tuple<uint32_t, int, int, int> Texture::query() const
+    {
+        uint32_t format;
+        int access, w, h;
+        if (SDL_QueryTexture(m_texture, &format, &access, &w, &h) < 0) {
+            throw std::runtime_error("Unable to query: " + std::string(SDL_GetError()));
+        }
+        return std::make_tuple(format, access, w, h);
+    }
+
+    std::tuple<uint8_t, uint8_t, uint8_t> Texture::get_color_mode() const
+    {
+        uint8_t r, g, b;
+        if(SDL_GetTextureColorMod(m_texture, &r, &g, &b) < 0) {
+            throw std::runtime_error("Unable to query color mode: " + std::string(SDL_GetError()));
+        }
+        return std::make_tuple(r,g,b);
+    }
+
 
 } // namespace GameEngine
