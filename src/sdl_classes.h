faa6244c7ebdd0db66a10b23b360dd469ff7a2db
andrey_dergunov <andrei_dergunov@epam.com>
2024-07-10 16:51:27 +0200

    add sdl_classes.h

---
+++ src/sdl_classes.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+namespace GameEngine {
+    // Copy of SDL_Rect
+    struct Rect {
+        int x, y;
+        int w, h;
+    };
+
+    struct RGBColor {
+        uint8_t r = 255;
+        uint8_t g = 255;
+        uint8_t b = 255;
+        uint8_t a = 255; //alpha: 255 - opaque, 0 - transparent
+    };
+
+    struct Pos2D {
+        int x;
+        int y;
+    };
+
+    struct Size2D {
+        size_t w;
+        size_t h;
+    };
+}
+
