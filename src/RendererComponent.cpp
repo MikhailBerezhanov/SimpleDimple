6beaee11b13ec68ead93c2888bfedfcb589be959
andrey_dergunov <andrei_dergunov@epam.com>
2024-08-06 12:46:32 +0200

    Renderer: texture calculation fixes, minor refactors

--- src/RendererComponent.cpp
+++ src/RendererComponent.cpp
@@ -34,7 +34,7 @@ namespace GameEngine {
         /// in case where we cannot distribute textures evenly by the lines, the last line's textures are widened
         const auto rem = m_texture_lines ? m_textures_q.size() % m_texture_lines : 0;
         m_tex_per_line_min = m_texture_lines ? m_textures_q.size() / m_texture_lines : 0;
-        m_tex_per_line_max = m_tex_per_line_min + rem ? 1 : 0;
+        m_tex_per_line_max = m_tex_per_line_min + (rem ? 1 : 0);
         m_tex_width_min = m_tex_per_line_max ? main_rect->w / m_tex_per_line_max : 0;
         m_tex_width_max = m_tex_per_line_min ? main_rect->w / m_tex_per_line_min : 0;
         m_tex_height = m_texture_lines ? main_rect->h / m_texture_lines : 0;
@@ -46,9 +46,13 @@ namespace GameEngine {
         /// Clear indexes
         m_line_idx = 0;
         m_tex_in_line_idx = 0;
+
+        m_offset = {main_rect->x, main_rect->y};
+        m_x_carry = 0;
+
     }
 
-    RendererComponent::TexRect RendererComponent::TextureHandle::get_texture_and_rect(const SDL_Rect *main_rect) {
+    RendererComponent::TexRect RendererComponent::TextureHandle::get_texture_and_rect() {
 
         TexRect res{};
 
@@ -77,8 +81,8 @@ namespace GameEngine {
             }
 
             res.m_rect = {
-                    static_cast<int>(main_rect->x + m_tex_in_line_idx * tex_width_in_this_line),
-                    static_cast<int>(main_rect->y + m_line_idx * tex_height_in_this_line),
+                    static_cast<int>(m_offset.x + m_x_carry),
+                    static_cast<int>(m_offset.y),
                     static_cast<int>(tex_width_in_this_line),
                     static_cast<int>(tex_height_in_this_line)
             };
@@ -87,8 +91,12 @@ namespace GameEngine {
 
             m_textures_q.pop();
 
+            m_x_carry += static_cast<int>(tex_width_in_this_line);
+
             if(++m_tex_in_line_idx >= tex_in_current_line) {
                 m_tex_in_line_idx = 0;
+                m_offset.y += static_cast<int>(tex_height_in_this_line);
+                m_x_carry = 0;
                 ++m_line_idx;
             }
         }
@@ -108,7 +116,7 @@ namespace GameEngine {
         m_textureHdl.calculate_texture_traits(main_rect);
 
         TexRect tex_rect{};
-        while((tex_rect = m_textureHdl.get_texture_and_rect(main_rect)).m_texture != nullptr) {
+        while((tex_rect = m_textureHdl.get_texture_and_rect()).m_texture != nullptr) {
 
             auto texture = tex_rect.m_texture->get_texture();
             const auto dst_rect = &tex_rect.m_rect;
