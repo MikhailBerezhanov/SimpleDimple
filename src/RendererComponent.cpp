e1a6ad09f25300f0498d57170527db927ed844e8
andrey_dergunov <andrei_dergunov@epam.com>
2024-08-10 16:12:30 +0200

    Renderer: define SetTextureRows

--- src/RendererComponent.cpp
+++ src/RendererComponent.cpp
@@ -263,6 +263,10 @@ namespace GameEngine {
         m_textureHdl.add_texture(tex);
     }
 
+    void RendererComponent::SetTextureRows(unsigned int lines) {
+        m_textureHdl.set_texture_lines(lines);
+    }
+
     void RendererComponent::OnUpdate() {
 
         update_textures();
