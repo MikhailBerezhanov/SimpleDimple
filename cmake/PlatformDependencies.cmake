1fd9c29f4e9bc359a10064db5c36ce88e2abf448
andrey_dergunov <andrei_dergunov@epam.com>
2024-08-24 12:09:01 +0200

    use function to find libs

--- cmake/PlatformDependencies.cmake
+++ cmake/PlatformDependencies.cmake
@@ -2,7 +2,7 @@
 # SDL version to link against
 set(SDL_USED_VERSION "SDL2")
 
-if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
+if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
     set(__SDL_PREFIX "/opt/sdl/windows")
     set(__ZLIB_PREFIX "/opt/zlib/windows")
     set(__LIBZIP_PREFIX "/opt/libzip/windows")
@@ -22,7 +22,7 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
     set(__SDL_DEFINITIONS main=SDL_main)
 else()
     # Linux
-    set(ENABLE_GTESTS TRUE)
+    set(ENABLE_GTESTS ON)
     set(__SDL_PREFIX "/opt/sdl/linux")
     set(__ZLIB_PREFIX "/opt/zlib/linux")
     set(__LIBZIP_PREFIX "/opt/libzip/linux")
@@ -44,35 +44,38 @@ set(__ZLIB_INCLUDE_DIR "${__ZLIB_PREFIX}/include")
 set(__LIBZIP_LIB_DIR "${__LIBZIP_PREFIX}/lib")
 set(__LIBZIP_INCLUDE_DIR "${__LIBZIP_PREFIX}/include")
 
-set(__SDL_LIBS "")
-
 set(SDL_LIB_DIR "${__SDL_PREFIX}/lib")
 set(SDL_BIN_DIR "${__SDL_PREFIX}/bin")
 
-# Collect all SDL libs
-foreach(X ${__SDL_LIB_NAMES})
-    find_library(__SDL_EL ${X} REQUIRED PATHS ${SDL_LIB_DIR} NO_DEFAULT_PATH)
-    list(APPEND __SDL_LIBS "${__SDL_EL}")
-    unset(__SDL_EL CACHE)
-endforeach()
+# Common function to find libraries
+function(find_libs OUTPUT_VAR LIB_NAMES LIB_PATH)
+    set(LIBS "")
+    foreach(LIB ${LIB_NAMES})
+        find_library(FOUND_LIB ${LIB} REQUIRED PATHS ${LIB_PATH} NO_DEFAULT_PATH)
+        list(APPEND LIBS "${FOUND_LIB}")
+        unset(FOUND_LIB CACHE)
+    endforeach()
+    set(${OUTPUT_VAR} ${LIBS} PARENT_SCOPE)
+endfunction()
 
+find_libs(__SDL_LIBS "${__SDL_LIB_NAMES}" "${SDL_LIB_DIR}")
 # Not used for now
-# find_library(__ZLIB_LIBS ${__ZLIB_LIB_NAMES} PATHS ${__ZLIB_LIB_DIR} NO_DEFAULT_PATH)
-# find_library(__LIBZIP_LIBS ${__LIBZIP_LIB_NAMES} PATHS ${__LIBZIP_LIB_DIR} NO_DEFAULT_PATH)
+#find_libs(__ZLIB_LIBS "${__ZLIB_LIB_NAMES}" "${__ZLIB_LIB_DIR}")
+#find_libs(__LIBZIP_LIBS "${__LIBZIP_LIB_NAMES}" "${__LIBZIP_LIB_DIR}")
 
 # A full collection of libs
 set(DEPENDENCY_LIBS 
     ${__SDL_EXTRA_LIBS} 
     ${__SDL_LIBS}
-    # ${__LIBZIP_LIBS}
-    # ${__ZLIB_LIBS}
+    ${__LIBZIP_LIBS}
+    ${__ZLIB_LIBS}
     ${__OTHER_LIBS}
 )
 
 # A full collection of includes
 set(DEPENDENCY_INCLUDE_DIRS
-    ${__SDL_INCLUDE_DIR} 
-    # ${__LIBZIP_INCLUDE_DIR}
+    ${__SDL_INCLUDE_DIR}
+    ${__LIBZIP_INCLUDE_DIR}
 )
 
 # A full list of compile definitions
